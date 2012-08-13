7c021bdc854c2bf6e17275ee344f5ab4b67cc78c
Erik Silkensen <silkense@colorado.edu>
2012-08-13 02:34:39 -0400

    adding make-error

--- utils.rkt
+++ utils.rkt
@@ -13,6 +13,19 @@
 
 (struct mark (name index line column buffer))
 
+(define (make-error type)
+  (λ (context problem problem-mark)
+    (error type "~a~a\n~a:~a:~a: ~a"
+           (if (string? context)
+               (format "~a;\n " context) "")
+           problem
+           (mark-name problem-mark)
+           (mark-line problem-mark)
+           (mark-column problem-mark)
+           (vector-ref
+            (mark-buffer problem-mark)
+            (mark-index problem-mark)))))
+
 (define (read-file filename)
   (with-input-from-file filename
     (λ ()
