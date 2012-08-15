fb45c3adb76f3a54297c8dcc6e7c75c61528bed1
Erik Silkensen <silkense@colorado.edu>
2012-08-15 09:49:33 -0400

    using yaml-struct: macro

--- events.rkt
+++ events.rkt
@@ -2,104 +2,42 @@
 
 #lang typed/racket
 
-(require "utils.rkt")
+(require "struct.rkt" "utils.rkt")
 
-(provide (except-out (all-defined-out) event-strings))
+(provide (all-defined-out))
 
-(struct: event ([start : mark] [end : mark]))
+(yaml-struct: event ([start : mark] [end : mark]))
 
-(: event-strings (HashTable (Any -> Boolean) (event -> String)))
-(define event-strings (make-hash))
-
-(: event->string (event -> String))
-(define (event->string event)
-  (let loop ([es (hash-keys event-strings)])
-    (if (null? es)
-        (error 'event->string "unexpected event type")
-        (if ((car es) event)
-            ((hash-ref event-strings (car es)) event)
-            (loop (cdr es))))))
-             
-(: print-event (event -> Void))
-(define (print-event event)
-  (displayln (event->string event)))
-
-(define-syntax (define-event: stx)
-  (define (build-name id . parts)
-    (let ([str (apply string-append
-                      (map (λ (p)
-                             (if (syntax? p)
-                                 (symbol->string (syntax-e p))
-                                 (format "~a" p)))
-                           parts))])
-      (datum->syntax id (string->symbol str) id)))
-  (syntax-case stx (:)
-    [(_ name [field : type] ...)
-     (let ([e (build-name #'name #'name "-event")]
-           [e? (build-name #'name #'name "-event?")]
-           [e->string (build-name #'name #'name "-event->string")]
-           [fs (map (λ (f) `(cons ,(format "~a" (syntax->datum f))
-                                  ,(build-name #'name #'name "-event-" f)))
-                    (sort (syntax->list #'(field ...))
-                          (λ (s t)
-                            (string<? (format "~a" (syntax->datum s))
-                                      (format "~a" (syntax->datum t))))))])
-       #`(begin
-           (struct: #,e event ([field : type] ...))
-           (: #,e->string (event -> String))
-           (define (#,e->string e)
-             (if (#,e? e)
-                 (let* ([attr->string
-                         (λ: ([p : (Pairof String (#,e -> Any))])
-                           (format "~a=~s" (car p) ((cdr p) e)))]
-                        [fields (map attr->string (list #,@fs))])
-                   (format "~a(~a)" '#,e (string-join fields ", ")))
-                 (error '#,e->string "unexpected event type")))
-           (hash-set! event-strings #,e? #,e->string)))]))
-
-(define-event: stream-start)
-
-(define-event: stream-end)
-
-(define-event: document-start
-  [explicit : Boolean]
-  [version : (Option (Pairof Integer Integer))]
-  [tags : (Option (HashTable String String))])
-
-(define-event: document-end
-  [explicit : Boolean])
-
-(define-event: alias
-  [anchor : (Option String)])
-
-(define-event: scalar
-  [anchor : (Option String)]
-  [tag : (Option String)]
-  [implicit : (Pairof Boolean Boolean)]
-  [value : String]
-  [style : (Option Char)])
-
-(define-event: sequence-start
-  [anchor : (Option String)]
-  [tag : (Option String)]
-  [implicit : Boolean]
-  [flow-style : Boolean])
-
-(define-event: sequence-end)
-
-(define-event: mapping-start
-  [anchor : (Option String)]
-  [tag : (Option String)]
-  [implicit : Boolean]
-  [flow-style : Boolean])
-
-(define-event: mapping-end)
+(event: stream-start)
+(event: stream-end)
+(event: document-start
+  ([explicit : Boolean]
+   [version : (Option (Pairof Integer Integer))]
+   [tags : (Option (HashTable String String))]))
+(event: document-end ([explicit : Boolean]))
+(event: alias ([anchor : (Option String)]))
+(event: scalar
+  ([anchor : (Option String)]
+   [tag : (Option String)]
+   [implicit : (Pairof Boolean Boolean)]
+   [value : String]
+   [style : (Option Char)]))
+(event: sequence-start
+  ([anchor : (Option String)]
+   [tag : (Option String)]
+   [implicit : Boolean]
+   [flow-style : Boolean]))
+(event: sequence-end)
+(event: mapping-start
+  ([anchor : (Option String)]
+   [tag : (Option String)]
+   [implicit : Boolean]
+   [flow-style : Boolean]))
+(event: mapping-end)
 
 (define-predicate node-event?
   (U alias-event scalar-event))
-
 (define-predicate collection-start-event?
   (U sequence-start-event mapping-start-event))
-
 (define-predicate collection-end-event?
   (U sequence-end-event mapping-end-event))
--- tokens.rkt
+++ tokens.rkt
@@ -2,105 +2,29 @@
 
 #lang typed/racket
 
-(require "utils.rkt")
+(require "struct.rkt" "utils.rkt")
 
-(provide (except-out (all-defined-out) token-strings))
+(provide (all-defined-out))
 
-(struct: token ([start : mark] [end : mark]))
+(yaml-struct: token ([start : mark] [end : mark]))
 
-(: token-strings (HashTable (Any -> Boolean) (token -> String)))
-(define token-strings (make-hash))
-
-(: token->string (token -> String))
-(define (token->string token)
-  (let loop ([ts (hash-keys token-strings)])
-    (if (null? ts)
-        (error 'token->string "unexpected token type")
-        (if ((car ts) token)
-            ((hash-ref token-strings (car ts)) token)
-            (loop (cdr ts))))))
-             
-(: print-token (token -> Void))
-(define (print-token token)
-  (displayln (token->string token)))
-
-(define-syntax (define-token: stx)
-  (define (build-name id . parts)
-    (let ([str (apply string-append
-                      (map (λ (p)
-                             (if (syntax? p)
-                                 (symbol->string (syntax-e p))
-                                 (format "~a" p)))
-                           parts))])
-      (datum->syntax id (string->symbol str) id)))
-  (syntax-case stx (:)
-    [(_ name [field : type] ...)
-     (let ([t (build-name #'name #'name "-token")]
-           [t? (build-name #'name #'name "-token?")]
-           [t->string (build-name #'name #'name "-token->string")]
-           [fs (map (λ (f) `(cons ,(format "~a" (syntax->datum f))
-                                  ,(build-name #'name #'name "-token-" f)))
-                    (sort (syntax->list #'(field ...))
-                          (λ (s t)
-                            (string<? (format "~a" (syntax->datum s))
-                                      (format "~a" (syntax->datum t))))))])
-       #`(begin
-           (struct: #,t token ([field : type] ...))
-           (: #,t->string (token -> String))
-           (define (#,t->string t)
-             (if (#,t? t)
-                 (let* ([attr->string
-                         (λ: ([p : (Pairof String (#,t -> Any))])
-                           (format "~a=~s" (car p) ((cdr p) t)))]
-                        [fields (map attr->string (list #,@fs))])
-                   (format "~a(~a)" '#,t (string-join fields ", ")))
-                 (error '#,t->string "unexpected token type")))
-           (hash-set! token-strings #,t? #,t->string)))]))
-
-(define-token: directive
-  [name : String]
-  [value : (Option (Pairof Integer Integer))])
-
-(define-token: document-start)
-
-(define-token: document-end)
-
-(define-token: stream-start)
-
-(define-token: stream-end)
-
-(define-token: block-sequence-start)
-
-(define-token: block-mapping-start)
-
-(define-token: block-end)
-
-(define-token: flow-sequence-start)
-
-(define-token: flow-mapping-start)
-
-(define-token: flow-sequence-end)
-
-(define-token: flow-mapping-end)
-
-(define-token: key)
-
-(define-token: value)
-
-(define-token: block-entry)
-
-(define-token: flow-entry)
-
-(define-token: alias
-  [value : String])
-
-(define-token: anchor
-  [value : String])
-
-(define-token: tag
-  [value : (Pairof (Option String) (Option String))])
-
-(define-token: scalar
-  [value : String]
-  [plain : Boolean]
-  [style : Char])
+(token: directive ([name : String] [value : (Option (Pair Integer Integer))]))
+(token: document-start)
+(token: document-end)
+(token: stream-start)
+(token: stream-end)
+(token: block-sequence-start)
+(token: block-mapping-start)
+(token: block-end)
+(token: flow-sequence-start)
+(token: flow-mapping-start)
+(token: flow-sequence-end)
+(token: flow-mapping-end)
+(token: key)
+(token: value)
+(token: block-entry)
+(token: flow-entry)
+(token: alias ([value : String]))
+(token: anchor ([value : String]))
+(token: tag ([value : (Pair (Option String) (Option String))]))
+(token: scalar ([value : String] [plain : Boolean] [style : Char]))
