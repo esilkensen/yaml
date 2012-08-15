fd08593235bb49a0f6081a1f5f11bc74d8f3eca5
Erik Silkensen <silkense@colorado.edu>
2012-08-15 10:16:16 -0400

    using yaml-struct: macro

--- nodes.rkt
+++ nodes.rkt
@@ -1,70 +1,22 @@
 ;;;;;; nodes.rkt - YAML nodes.    -*- Mode: Racket -*-
 
-#lang racket
+#lang typed/racket
+
+(require "struct.rkt" "utils.rkt")
 
 (provide (all-defined-out))
 
-(struct node (type tag value start end attrs) #:mutable)
+(yaml-struct: node ([start : mark] [end : (Option mark)]) #:mutable)
 
-(define (node->string node)
-  (cond
-   [(node? node)
-    (let ([tag (node-tag node)]
-          [value (node-value node)]
-          [start (node-start node)]
-          [end (node-end node)]
-          [attrs (node-attrs node)])
-      (format "~a-node(~a)"
-              (node-type node)
-              (string-join
-               (map
-                (λ (attr)
-                  (format "~a=~s" attr (hash-ref attrs attr)))
-                (sort (hash-keys attrs)
-                      (λ (x y)
-                        (string<=? (symbol->string x)
-                                   (symbol->string y)))))
-               ", ")))]
-   [else "no node!"]))
-
-(define (print-node node)
-  (displayln (node->string node)))
-
-(define-syntax (define-node stx)
-  (define (build-name id . parts)
-    (let ([str (apply string-append
-                      (map (λ (p)
-                             (if (syntax? p)
-                                 (symbol->string (syntax-e p))
-                                 (format "~a" p)))
-                           parts))])
-      (datum->syntax id (string->symbol str) id)))
-  (syntax-case stx ()
-    [(_ name field ...)
-     (let ([n (build-name #'name #'name "-node")]
-           [n? (build-name #'name #'name "-node?")]
-           [fs (map (λ (f)
-                      (build-name #'name #'name "-node-" f))
-                    (syntax->list #'(field ...)))])
-       #`(begin
-           (define (#,n tag value start end field ...)
-             (let ([attrs (make-hash `((field . ,field) ...))])
-               (node 'name start end attrs)))
-           (define (#,n? node)
-             (and (node? node)
-                  (eq? 'name (node-type node))))
-           (define-values (#,@fs)
-             (values
-              (λ (node)
-                (hash-ref (node-attrs node) 'field)) ...))))]))
-
-(define-node scalar style)
-(define-node collection flow-style)
-(define-node sequence flow-style) ; collection
-(define-node mapping flow-style) ; collection
-
-(define (any-collection-node? n)
-  (or (collection-node? n)
-      (sequence-node? n)
-      (mapping-node? n)))
-
+(node: scalar
+  ([tag : (Option String)]
+   [value : String]
+   [style : Boolean]))
+(node: sequence
+  ([tag : (Option String)]
+   [value : (Listof node)]
+   [flow-style : Boolean]))
+(node: mapping
+  ([tag : (Option String)]
+   [value : (Listof node)]
+   [flow-style : Boolean]))
