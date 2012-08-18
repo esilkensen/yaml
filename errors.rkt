ad7737aeb669d4901c18de3beb40005deb4dcabc
Erik Silkensen <silkense@colorado.edu>
2012-08-18 02:21:20 -0400

    adding errors.rkt

---
+++ errors.rkt
@@ -0,0 +1,23 @@
+;;;;;; errors.rkt - YAML errors.    -*- Mode: Racket -*-
+
+#lang typed/racket
+
+(provide (all-defined-out))
+
+(struct: mark
+  ([name : String] [index : Integer] [line : Integer]
+   [column : Integer] [buffer : (Vectorof (U Char EOF))]))
+
+(: make-error (Symbol -> ((Option String) String mark -> Nothing)))
+(define (make-error type)
+  (λ (context problem problem-mark)
+    (error type "~a~a\n~a:~a:~a: ~a"
+           (if (string? context)
+               (format "~a;\n " context) "")
+           problem
+           (mark-name problem-mark)
+           (mark-line problem-mark)
+           (mark-column problem-mark)
+           (vector-ref
+            (mark-buffer problem-mark)
+            (mark-index problem-mark)))))
