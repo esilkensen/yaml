1481669c93d4db4ed6ef0665bd8aacacdb9e297a
Erik Silkensen <silkense@colorado.edu>
2012-08-25 04:57:18 -0400

    moving yaml-null parameter to private

--- main.rkt
+++ main.rkt
@@ -5,77 +5,49 @@
 (require
  "private/constructor.rkt"
  "private/representer.rkt"
- "private/serializer.rkt")
+ "private/serializer.rkt"
+ "private/yaml.rkt")
 
 (provide
+ (all-from-out "private/yaml.rkt")
  (contract-out
-  [yaml? (-> any/c boolean?)]
-  [yaml-null (case-> (-> any/c) (-> any/c void?))]
   [read-yaml
-   (->* () (input-port? any/c #:null any/c) yaml?)]
+   (->* () (any/c input-port?) yaml?)]
   [read-yaml*
-   (->* () (input-port? any/c #:null any/c) (listof yaml?))]
+   (->* () (any/c input-port?) (listof yaml?))]
   [string->yaml
-   (->* (string?) (#:null any/c) yaml?)]
+   (-> string? yaml?)]
   [string->yaml*
-   (->* (string?) (#:null any/c) (listof yaml?))]
+   (-> string? (listof yaml?))]
   [write-yaml
    (->* (yaml?)
         (output-port?
-         #:null any/c
          #:style (or/c #f char?)
          #:flow-style (or/c boolean? 'best))
         void?)]
   [write-yaml*
    (->* ((listof yaml?))
         (output-port?
-         #:null any/c
          #:style (or/c #f char?)
          #:flow-style (or/c boolean? 'best))
         void?)]
   [yaml->string
    (->* (yaml?)
-        (#:null any/c
-         #:style (or/c #f char?)
+        (#:style (or/c #f char?)
          #:flow-style (or/c boolean? 'best))
         string?)]
   [yaml*->string
    (->* ((listof yaml?))
-        (#:null any/c
-         #:style (or/c #f char?)
+        (#:style (or/c #f char?)
          #:flow-style (or/c boolean? 'best))
         (listof string?))]))
 
-(define yaml-null (make-parameter 'null))
-
-(define (yaml? v)
-  (or (equal? v (yaml-null))
-      (string? v)
-      (boolean? v)
-      (exact-integer? v)
-      (inexact-real? v)
-      (date? v)
-      (and (list? v)
-           (andmap yaml? v))
-      (and (hash? v)
-           (for/and ([(key val) v])
-             (and (yaml? key)
-                  (yaml? val))))
-      (and (set? v)
-           (for/and ([val v])
-             (yaml? val)))
-      (and (pair? v)
-           (yaml? (car v))
-           (yaml? (cdr v)))))
-
-(define (read-yaml [name 'input] [in (current-input-port)]
-                   #:null [null (yaml-null)])
+(define (read-yaml [name 'input] [in (current-input-port)])
   (define-values (check-data? get-data get-single-data)
     (make-constructor name in))
   (get-single-data))
 
-(define (read-yaml* [name 'input] [in (current-input-port)]
-                    #:null [null (yaml-null)])
+(define (read-yaml* [name 'input] [in (current-input-port)])
   (define-values (check-data? get-data get-single-data)
     (make-constructor name in))
   (let loop ([docs '()])
@@ -83,25 +55,22 @@
         (loop (cons (get-data) docs))
         (reverse docs))))
 
-(define (string->yaml str #:null [null (yaml-null)])
+(define (string->yaml str)
   (with-input-from-string str
-    (λ () (read-yaml 'string #:null null))))
+    (λ () (read-yaml 'string))))
 
-(define (string->yaml* str #:null [null (yaml-null)])
+(define (string->yaml* str)
   (with-input-from-string str
-    (λ () (read-yaml* 'string #:null null))))
+    (λ () (read-yaml* 'string))))
 
 (define (write-yaml document [out (current-output-port)]
-                    #:null [null (yaml-null)]
                     #:style [default-style #f]
                     #:flow-style [default-flow-style 'best])
   (write-yaml* (list document) out
-               #:null null
                #:style default-style
                #:flow-style default-flow-style))
 
 (define (write-yaml* documents [out (current-output-port)]
-                     #:null [null (yaml-null)]
                      #:style [default-style #f]
                      #:flow-style [default-flow-style 'best])
   (define-values (open close serialize)
@@ -114,23 +83,19 @@
   (close))
 
 (define (yaml->string document
-                      #:null [null (yaml-null)]
                       #:style [default-style #f]
                       #:flow-style [default-flow-style 'best])
   (with-output-to-string
     (λ ()
       (write-yaml document
-                  #:null null
                   #:style default-style
                   #:flow-style default-flow-style))))
 
 (define (yaml*->string documents
-                       #:null [null (yaml-null)]
                        #:style [default-style #f]
                        #:flow-style [default-flow-style 'best])
   (with-output-to-string
     (λ ()
       (write-yaml* documents
-                   #:null null
                    #:style default-style
                    #:flow-style default-flow-style))))
