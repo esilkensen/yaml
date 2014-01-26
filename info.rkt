775b745c0bf246e00d608daf579e7e9e8306893b
Erik Silkensen <ejs@ccs.neu.edu>
2014-01-26 14:07:02 -0500

    trying to turn yaml into a pkg

--- info.rkt
+++ info.rkt
@@ -1,10 +1,3 @@
 #lang setup/infotab
 (define name "YAML")
-(define blurb '("A YAML parser and emitter for Racket."))
-(define primary-file "main.rkt")
-(define categories '(net datastructures misc))
-(define repositories '("4.x"))
-(define required-core-version "5.3")
 (define scribblings '(("yaml.scrbl" (user-doc) (parsing-library))))
-(define release-notes
-  '("Minor update to documentation."))
