b1bd653ae7d3365ef70583cdc69183df0de2b0db
Erik Silkensen <silkense@colorado.edu>
2016-04-24 22:39:36 -0600

    initial travis configuration files

--- info.rkt
+++ info.rkt
@@ -2,4 +2,6 @@
 
 (define collection 'multi)
 (define deps '("base" "srfi-lite-lib" "typed-racket-lib"))
-(define build-deps '("rackunit-lib" "scribble-lib" "racket-doc" "sandbox-lib"))
+(define build-deps
+  '("rackunit-lib" "scribble-lib" "racket-doc" "sandbox-lib"
+    "cover-coveralls"))
