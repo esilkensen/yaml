aa9a6e3a938882355b742af7779622e38a60e1c9
Erik Silkensen <silkense@colorado.edu>
2016-11-03 01:19:06 -0600

    Refactor representer and serializer as classes

--- yaml/representer.rkt
+++ yaml/representer.rkt
@@ -7,201 +7,213 @@
  (except-in srfi/13 string-replace)
  "errors.rkt"
  "nodes.rkt"
+ "serializer.rkt"
  "utils.rkt"
  "yaml-expr.rkt")
 
 (provide
+ representer+c%
  (contract-out
-  [make-representer
-   (((node? . -> . void?))
-    (#:scalar-style (or/c #\" #\' #\| #\> 'plain)
-     #:style (or/c 'block 'flow 'best)
-     #:sort-mapping (or/c (any/c any/c . -> . any/c) #f)
-     #:sort-mapping-key (any/c . -> . any/c))
-    . ->* .
-    ;; represent
-    (yaml? . -> . void?))]))
+  [representer% representer+c%]))
+
+(define representer+c%
+  (class/c
+   (init-field
+    [serializer (instanceof/c serializer+c%)]
+    [scalar-style (or/c #\" #\' #\| #\> #f)]
+    [style (or/c 'block 'flow 'best)]
+    [sort-mapping (or/c (any/c any/c . -> . any/c) #f)]
+    [sort-mapping-key (any/c . -> . any/c)])
+   [represent (yaml? . ->m . void?)]
+   [add ((any/c . -> . boolean?) (any/c . -> . node?) . ->m . void?)]))
 
 (define (representer-error message)
   (error 'representer message))
 
-(define (make-representer serialize
-                          #:scalar-style [default-style 'plain]
-                          #:style [default-flow-style 'best]
-                          #:sort-mapping [mapping-less-than? #f]
-                          #:sort-mapping-key [mapping-extract-key identity])
-  (define yaml-representers '())
-  (define represented-objects (make-hash))
-  (define object-keeper '())
-  (define alias-key #f)
-  
-  (define (represent data)
-    (serialize (represent-data data))
-    (set! represented-objects (make-hash))
-    (set! object-keeper '())
-    (set! alias-key #f))
-  
-  (define (represent-data data)
-    (call/cc
-     (λ (return)
-       (if (ignore-aliases? data)
-           (set! alias-key #f)
-           (set! alias-key (eq-hash-code data)))
-       (when alias-key
-         (when (hash-has-key? represented-objects alias-key)
-           (return (hash-ref represented-objects alias-key)))
-         (append! object-keeper (list data)))
-       (let loop ([kvs yaml-representers])
-         (if (null? kvs)
-             (scalar-node #f #f #f data #f)
-             (let ([type? (caar kvs)] [repr (cdar kvs)])
-               (if (type? data)
-                   (repr data)
-                   (loop (cdr kvs)))))))))
-  
-  (define (represent-scalar tag value [style #f])
-    (unless style
-      (set! style (if (eq? 'plain default-style) #f default-style)))
-    (let ([node (scalar-node #f #f tag value style)])
+(define representer%
+  (class object%
+    (init-field
+     serializer
+     [scalar-style #f]
+     [style 'best]
+     [sort-mapping #f]
+     [sort-mapping-key identity])
+    
+    (super-new)
+    
+    (define yaml-representers '())
+    (define represented-objects (make-hash))
+    (define object-keeper '())
+    (define alias-key #f)
+    
+    (define/public (represent data)
+      (send serializer serialize (represent-data data))
+      (set! represented-objects (make-hash))
+      (set! object-keeper '())
+      (set! alias-key #f))
+
+    (define (represent-data data)
+      (call/cc
+       (λ (return)
+         (if (ignore-aliases? data)
+             (set! alias-key #f)
+             (set! alias-key (eq-hash-code data)))
+         (when alias-key
+           (when (hash-has-key? represented-objects alias-key)
+             (return (hash-ref represented-objects alias-key)))
+           (append! object-keeper (list data)))
+         (let loop ([kvs yaml-representers])
+           (if (null? kvs)
+               (scalar-node #f #f #f data #f)
+               (let ([type? (caar kvs)] [repr (cdar kvs)])
+                 (if (type? data)
+                     (repr data)
+                     (loop (cdr kvs)))))))))
+    
+    (define (represent-scalar tag value)
+      (define node (scalar-node #f #f tag value scalar-style))
       (when alias-key
         (hash-set! represented-objects alias-key node))
-      node))
-  
-  (define (represent-sequence tag sequence [flow-style #f])
-    (let ([value '()]
-          [best-style #t])
-      (for ([item sequence])
-        (let ([node-item (represent-data item)])
-          (when (not (and (scalar-node? node-item)
-                          (not (scalar-node-style node-item))))
-            (set! best-style #f))
-          (append! value (list node-item))))
-      (unless flow-style
-        (if (eq? 'best default-flow-style)
-            (set! flow-style best-style)
-            (set! flow-style (eq? 'flow default-flow-style))))
-      (let ([node (sequence-node #f #f tag value flow-style)])
-        (when alias-key
-          (hash-set! represented-objects alias-key node))
-        node)))
-  
-  (define (represent-mapping tag mapping [flow-style #f])
-    (let ([value '()]
-          [best-style #t]
-          [key-vals (hash->list mapping)])
-      (when mapping-less-than?
-        (set! key-vals (sort key-vals
-                             mapping-less-than?
-                             #:key mapping-extract-key)))
-      (for ([key-val key-vals])
-        (let* ([node-key (represent-data (car key-val))]
-               [node-value (represent-data (cdr key-val))])
-          (when (not (and (scalar-node? node-key)
-                          (not (scalar-node-style node-key))))
-            (set! best-style #f))
-          (when (not (and (scalar-node? node-value)
-                          (not (scalar-node-style node-value))))
-            (set! best-style #f))
-          (append! value (list (cons node-key node-value)))))
-      (unless flow-style
-        (if (eq? 'best default-flow-style)
-            (set! flow-style best-style)
-            (set! flow-style (eq? 'flow default-flow-style))))
-      (let ([node (mapping-node #f #f tag value flow-style)])
-        (when alias-key
-          (hash-set! represented-objects alias-key node))
-        node)))
-  
-  (define (ignore-aliases? data)
-    (or (equal? (yaml-null) data)
-        (boolean? data)
-        (string? data)
-        (number? data)))
-  
-  (define (represent-null data)
-    (represent-scalar "tag:yaml.org,2002:null" "null"))
-  
-  (define (represent-str data)
-    (represent-scalar "tag:yaml.org,2002:str" data))
+      node)
 
-  (define (represent-binary data)
-    (represent-scalar "tag:yaml.org,2002:binary" (bytes->string/utf-8 data)))
-  
-  (define (represent-bool data)
-    (represent-scalar "tag:yaml.org,2002:bool" (if data "true" "false")))
-  
-  (define (represent-int data)
-    (represent-scalar "tag:yaml.org,2002:int" (number->string data)))
-  
-  (define (represent-float data)
-    (represent-scalar
-     "tag:yaml.org,2002:float"
-     (cond
-       [(nan? data) ".nan"]
-       [(infinite? data)
-        (if (equal? data +inf.0) ".inf" "-.inf")]
-       [else
-        (let ([value (number->string data)])
-          (if (and (not (string-index value #\.))
-                   (string-index value #\e))
-              (string-replace value "e" ".0e" #:all? #f)
-              value))])))
-  
-  (define (represent-list data)
-    (represent-sequence "tag:yaml.org,2002:seq" data))
-  
-  (define (represent-hash data)
-    (represent-mapping "tag:yaml.org,2002:map" data))
-  
-  (define (represent-set data)
-    (let ([value (make-hash)])
-      (for ([key data])
-        (hash-set! value key (yaml-null)))
-      (represent-mapping "tag:yaml.org,2002:set" value)))
-  
-  (define (represent-date data)
-    (parameterize ([date-display-format 'iso-8601])
-      ;; this isn't perfect, but it'll have to do...
-      (let* ([time? (or (not (zero? (date-second data)))
-                        (not (zero? (date-minute data)))
-                        (not (zero? (date-hour data))))]
-             [value (date->string data time?)])
-        (when (and (date*? data)
-                   (not (zero? (date*-nanosecond data))))
-          (let ([microsecond (/ (date*-nanosecond data) 1000)])
-            (set! value (format "~a.~a" value microsecond))))
-        (represent-scalar "tag:yaml.org,2002:timestamp" value))))
-  
-  (define (represent-pair data)
-    (let ([value (list (car data) (cdr data))])
-      (represent-sequence "tag:yaml.org,2002:pair" value)))
-  
-  (define (represent-struct data)
-    (define-values (struct-type skipped?)
-      (struct-info data))
-    (define-values (name a b c d e f g)
-      (if skipped?
-          (representer-error
-           (format "~a not a transparent struct" (pretty-format data)))
-          (struct-type-info struct-type)))
-    (represent-mapping
-     (format "tag:yaml.org,2002:struct:~a" name)
-     (make-hash (gen->yaml data))))
-  
-  (define (add-representer! type? representer)
-    (append! yaml-representers (list (cons type? representer))))
-  
-  (add-representer! (λ (x) (equal? x (yaml-null))) represent-null)
-  (add-representer! string? represent-str)
-  (add-representer! bytes? represent-binary)
-  (add-representer! boolean? represent-bool)
-  (add-representer! integer? represent-int)
-  (add-representer! real? represent-float)
-  (add-representer! list? represent-list)
-  (add-representer! hash? represent-hash)
-  (add-representer! set? represent-set)
-  (add-representer! date? represent-date)
-  (add-representer! pair? represent-pair)
-  (add-representer! yaml-struct? represent-struct)
-  
-  (values represent))
+    (define (represent-sequence tag sequence)
+      (define best-style #t)
+      (define value
+        (for/list ([item sequence])
+          (let-values
+              ([(node-item flow-style?)
+                (represent-data/flow-style? item)])
+            (when flow-style?
+              (set! best-style #f))
+            node-item)))
+      (define node (sequence-node #f #f tag value (get-style best-style)))
+      (when alias-key
+        (hash-set! represented-objects alias-key node))
+      node)
+    
+    (define (represent-mapping tag mapping)
+      (define best-style #t)
+      (define sequence
+        (if sort-mapping
+            (sort (hash->list mapping)
+                  sort-mapping
+                  #:key sort-mapping-key)
+            (hash->list mapping)))
+      (define value
+        (for/list ([item sequence])
+          (let-values
+              ([(node-key key-flow-style?)
+                (represent-data/flow-style? (car item))]
+               [(node-value value-flow-style?)
+                (represent-data/flow-style? (cdr item))])
+            (when (or key-flow-style? value-flow-style?)
+              (set! best-style #f))
+            (cons node-key node-value))))
+      (define node (mapping-node #f #f tag value (get-style best-style)))
+      (when alias-key
+        (hash-set! represented-objects alias-key node))
+      node)
+    
+    (define (represent-data/flow-style? data)
+      (define node (represent-data data))
+      (define flow-style?
+        (not (and (scalar-node? node)
+                  (not (scalar-node-style node)))))
+      (values node flow-style?))
+    
+    (define (get-style best-style)
+      (if (eq? 'best style)
+          best-style
+          (eq? 'flow style)))
+    
+    (define (ignore-aliases? data)
+      (or (equal? (yaml-null) data)
+          (boolean? data)
+          (string? data)
+          (number? data)))
+    
+    (define (represent-null data)
+      (represent-scalar "tag:yaml.org,2002:null" "null"))
+    
+    (define (represent-str data)
+      (represent-scalar "tag:yaml.org,2002:str" data))
+    
+    (define (represent-binary data)
+      (represent-scalar "tag:yaml.org,2002:binary" (bytes->string/utf-8 data)))
+    
+    (define (represent-bool data)
+      (represent-scalar "tag:yaml.org,2002:bool" (if data "true" "false")))
+    
+    (define (represent-int data)
+      (represent-scalar "tag:yaml.org,2002:int" (number->string data)))
+    
+    (define (represent-float data)
+      (represent-scalar
+       "tag:yaml.org,2002:float"
+       (cond
+         [(nan? data) ".nan"]
+         [(infinite? data)
+          (if (equal? data +inf.0) ".inf" "-.inf")]
+         [else
+          (let ([value (number->string data)])
+            (if (and (not (string-index value #\.))
+                     (string-index value #\e))
+                (string-replace value "e" ".0e" #:all? #f)
+                value))])))
+    
+    (define (represent-list data)
+      (represent-sequence "tag:yaml.org,2002:seq" data))
+    
+    (define (represent-hash data)
+      (represent-mapping "tag:yaml.org,2002:map" data))
+    
+    (define (represent-set data)
+      (let ([value (make-hash)])
+        (for ([key data])
+          (hash-set! value key (yaml-null)))
+        (represent-mapping "tag:yaml.org,2002:set" value)))
+    
+    (define (represent-date data)
+      (parameterize ([date-display-format 'iso-8601])
+        ;; this isn't perfect, but it'll have to do...
+        (let* ([time? (or (not (zero? (date-second data)))
+                          (not (zero? (date-minute data)))
+                          (not (zero? (date-hour data))))]
+               [value (date->string data time?)])
+          (when (and (date*? data)
+                     (not (zero? (date*-nanosecond data))))
+            (let ([microsecond (/ (date*-nanosecond data) 1000)])
+              (set! value (format "~a.~a" value microsecond))))
+          (represent-scalar "tag:yaml.org,2002:timestamp" value))))
+    
+    (define (represent-pair data)
+      (let ([value (list (car data) (cdr data))])
+        (represent-sequence "tag:yaml.org,2002:pair" value)))
+    
+    (define (represent-struct data)
+      (define-values (struct-type skipped?)
+        (struct-info data))
+      (define-values (name a b c d e f g)
+        (if skipped?
+            (representer-error
+             (format "~a not a transparent struct" (pretty-format data)))
+            (struct-type-info struct-type)))
+      (represent-mapping
+       (format "tag:yaml.org,2002:struct:~a" name)
+       (make-hash (gen->yaml data))))
+    
+    (define/public (add type? representer)
+      (append! yaml-representers (list (cons type? representer))))
+    
+    (add (λ (x) (equal? x (yaml-null))) represent-null)
+    (add string? represent-str)
+    (add bytes? represent-binary)
+    (add boolean? represent-bool)
+    (add integer? represent-int)
+    (add real? represent-float)
+    (add list? represent-list)
+    (add hash? represent-hash)
+    (add set? represent-set)
+    (add date? represent-date)
+    (add pair? represent-pair)
+    (add yaml-struct? represent-struct)))
--- yaml/serializer.rkt
+++ yaml/serializer.rkt
@@ -11,165 +11,181 @@
  "utils.rkt")
 
 (provide
+ serializer+c%
  (contract-out
-  [make-serializer
-   (()
-    (output-port?
-     #:canonical boolean?
-     #:indent (or/c exact-positive-integer? #f)
-     #:width (or/c exact-positive-integer? #f)
-     #:allow-unicode boolean?
-     #:line-break (or/c "\n" "\r" "\r\n" #f)
-     #:explicit-start boolean?
-     #:explicit-end boolean?
-     #:version (or/c (cons/c exact-integer? exact-integer?) #f)
-     #:tags (or/c (hash/c string? string?) #f))
-    . ->* .
-    (values
-     ;; open
-     (-> void?)
-     ;; close
-     (-> void?)
-     ;; serialize
-     (node? . -> . void?)))]))
+  [serializer% serializer+c%]))
+
+(define serializer+c%
+  (class/c
+   (init-field
+    [out output-port?]
+    [canonical boolean?]
+    [indent (or/c exact-positive-integer? #f)]
+    [width (or/c exact-positive-integer? #f)]
+    [allow-unicode boolean?]
+    [line-break (or/c "\n" "\r" "\r\n" #f)]
+    [explicit-start boolean?]
+    [explicit-end boolean?]
+    [version (or/c (cons/c exact-integer? exact-integer?) #f)]
+    [tags (or/c (hash/c string? string?) #f)])
+   [open (->m void?)]
+   [close (->m void?)]
+   [serialize (node? . ->m . void?)]))
 
 (define ANCHOR-TEMPLATE "id~a")
 
 (define (serializer-error msg) (error 'serializer msg))
 
-(define (make-serializer [out (current-output-port)]
-                         #:canonical [canonical #f]
-                         #:indent [default-indent #f]
-                         #:width [default-width #f]
-                         #:allow-unicode [allow-unicode #f]
-                         #:line-break [line-break #f]
-                         #:explicit-start [explicit-start #f]
-                         #:explicit-end [explicit-end #f]
-                         #:version [version #f]
-                         #:tags [tags #f])
-  (define emit (make-emitter out
-                             #:canonical canonical
-                             #:indent default-indent
-                             #:width default-width
-                             #:allow-unicode allow-unicode
-                             #:line-break line-break))
-  (define serialized-nodes (make-hasheq))
-  (define anchors (make-hasheq))
-  (define last-anchor-id 0)
-  (define closed '())
-  
-  (define (open)
-    (cond
-      [(null? closed)
-       (emit (stream-start-event #f #f))
-       (set! closed #f)]
-      [closed
-       (serializer-error "serializer is closed")]
-      [else
-       (serializer-error "serializer is open")]))
-  
-  (define (close)
-    (cond
-      [(null? closed)
-       (serializer-error "serializer has not been opened")]
-      [(not closed)
-       (emit (stream-end-event #f #f))
-       (set! closed #t)]
-      [else
-       (serializer-error "serializer is closed")]))
-  
-  (define (serialize node)
-    (cond
-      [(null? closed)
-       (serializer-error "serializer has not been opened")]
-      [closed
-       (serializer-error "serializer is closed")])
-    (emit (document-start-event #f #f explicit-start version tags))
-    (anchor-node node)
-    (serialize-node node)
-    (emit (document-end-event #f #f explicit-end))
-    (set! serialized-nodes (make-hasheq))
-    (set! anchors (make-hasheq))
-    (set! last-anchor-id 0))
-  
-  (define (anchor-node node)
-    (cond
-      [(hash-has-key? anchors node)
-       (unless (hash-ref anchors node)
-         (hash-set! anchors node (generate-anchor node)))]
-      [else
-       (hash-set! anchors node #f)
-       (cond
-         [(sequence-node? node)
-          (for ([item (sequence-node-value node)])
-            (anchor-node item))]
-         [(mapping-node? node)
-          (for ([kv (mapping-node-value node)])
-            (anchor-node (car kv))
-            (anchor-node (cdr kv)))])]))
-  
-  (define (generate-anchor node)
-    (set! last-anchor-id (add1 last-anchor-id))
-    (let ([str (number->string last-anchor-id)])
-      (while (< (string-length str) 3)
-        (set! str (string-append "0" str)))
-      (format ANCHOR-TEMPLATE str)))
-  
-  (define (serialize-node node)
-    (let ([alias (hash-ref anchors node)])
+(define serializer%
+  (class object%
+    (init-field
+     [out (current-output-port)]
+     [canonical #f]
+     [indent #f]
+     [width #f]
+     [allow-unicode #f]
+     [line-break #f]
+     [explicit-start #f]
+     [explicit-end #f]
+     [version #f]
+     [tags #f])
+    
+    (super-new)
+    
+    (define emit (make-emitter out
+                               #:canonical canonical
+                               #:indent indent
+                               #:width width
+                               #:allow-unicode allow-unicode
+                               #:line-break line-break))
+    (define serialized-nodes (make-hasheq))
+    (define anchors (make-hasheq))
+    (define last-anchor-id 0)
+    (define closed '())
+    
+    (define/public (open)
       (cond
-        [(hash-has-key? serialized-nodes node)
-         (emit (alias-event #f #f alias))]
+        [(null? closed)
+         (emit (stream-start-event #f #f))
+         (set! closed #f)]
+        [closed
+         (serializer-error "serializer is closed")]
         [else
-         (hash-set! serialized-nodes node #t)
+         (serializer-error "serializer is open")]))
+    
+    (define/public (close)
+      (cond
+        [(null? closed)
+         (serializer-error "serializer has not been opened")]
+        [(not closed)
+         (emit (stream-end-event #f #f))
+         (set! closed #t)]
+        [else
+         (serializer-error "serializer is closed")]))
+    
+    (define/public (serialize node)
+      (cond
+        [(null? closed)
+         (serializer-error "serializer has not been opened")]
+        [closed
+         (serializer-error "serializer is closed")])
+      (emit (document-start-event #f #f explicit-start version tags))
+      (anchor-node node)
+      (serialize-node node)
+      (emit (document-end-event #f #f explicit-end))
+      (set! serialized-nodes (make-hasheq))
+      (set! anchors (make-hasheq))
+      (set! last-anchor-id 0))
+    
+    (define (anchor-node node)
+      (cond
+        [(hash-has-key? anchors node)
+         (unless (hash-ref anchors node)
+           (hash-set! anchors node (generate-anchor node)))]
+        [else
+         (hash-set! anchors node #f)
          (cond
-           [(scalar-node? node)
-            (let* ([tag (scalar-node-tag node)]
-                   [value (scalar-node-value node)]
-                   [style (scalar-node-style node)]
-                   [detected-tag
-                    (resolve 'scalar value (cons #t #f))]
-                   [default-tag
-                     (resolve 'scalar value (cons #f #t))]
-                   [implicit (cons (equal? tag detected-tag)
-                                   (equal? tag default-tag))])
-              (emit (scalar-event #f #f alias tag implicit value style)))]
            [(sequence-node? node)
-            (let* ([tag (sequence-node-tag node)]
-                   [value (sequence-node-value node)]
-                   [implicit (equal? tag (resolve 'sequence value #t))]
-                   [flow-style (sequence-node-flow-style node)])
-              (emit (sequence-start-event #f #f alias tag implicit flow-style))
-              (for ([item value])
-                (serialize-node item))
-              (emit (sequence-end-event #f #f)))]
+            (for ([item (sequence-node-value node)])
+              (anchor-node item))]
            [(mapping-node? node)
-            (let* ([tag (mapping-node-tag node)]
-                   [value (mapping-node-value node)]
-                   [implicit (equal? tag (resolve 'mapping value #t))]
-                   [flow-style (mapping-node-flow-style node)])
-              (emit
-               (mapping-start-event #f #f alias tag implicit flow-style))
-              (for ([kv value])
-                (serialize-node (car kv))
-                (serialize-node (cdr kv)))
-              (emit (mapping-end-event #f #f)))])])))
-  
-  (values open close serialize))
+            (for ([kv (mapping-node-value node)])
+              (anchor-node (car kv))
+              (anchor-node (cdr kv)))])]))
+    
+    (define (generate-anchor node)
+      (set! last-anchor-id (add1 last-anchor-id))
+      (let ([str (number->string last-anchor-id)])
+        (while (< (string-length str) 3)
+          (set! str (string-append "0" str)))
+        (format ANCHOR-TEMPLATE str)))
+    
+    (define (serialize-node node)
+      (let ([alias (hash-ref anchors node)])
+        (cond
+          [(hash-has-key? serialized-nodes node)
+           (emit (alias-event #f #f alias))]
+          [else
+           (hash-set! serialized-nodes node #t)
+           (cond
+             [(scalar-node? node)
+              (let* ([tag (scalar-node-tag node)]
+                     [value (scalar-node-value node)]
+                     [style (scalar-node-style node)]
+                     [detected-tag
+                      (resolve 'scalar value (cons #t #f))]
+                     [default-tag
+                       (resolve 'scalar value (cons #f #t))]
+                     [implicit (cons (equal? tag detected-tag)
+                                     (equal? tag default-tag))])
+                (emit (scalar-event #f #f alias tag implicit value style)))]
+             [(sequence-node? node)
+              (let* ([tag (sequence-node-tag node)]
+                     [value (sequence-node-value node)]
+                     [implicit (equal? tag (resolve 'sequence value #t))]
+                     [flow-style (sequence-node-flow-style node)])
+                (emit
+                 (sequence-start-event #f #f alias tag implicit flow-style))
+                (for ([item value])
+                  (serialize-node item))
+                (emit (sequence-end-event #f #f)))]
+             [(mapping-node? node)
+              (let* ([tag (mapping-node-tag node)]
+                     [value (mapping-node-value node)]
+                     [implicit (equal? tag (resolve 'mapping value #t))]
+                     [flow-style (mapping-node-flow-style node)])
+                (emit
+                 (mapping-start-event #f #f alias tag implicit flow-style))
+                (for ([kv value])
+                  (serialize-node (car kv))
+                  (serialize-node (cdr kv)))
+                (emit (mapping-end-event #f #f)))])])))))
 
 (module+ test
   (require rackunit)
   (test-case "open"
-    (define-values (open close serialize) (make-serializer))
-    (open)
-    (check-exn #rx"serializer is open" open)
-    (close)
-    (check-exn #rx"serializer is closed" open))
+    (define serializer (new serializer%))
+    (send serializer open)
+    (check-exn
+     #rx"serializer is open"
+     (λ () (send serializer open)))
+    (send serializer close)
+    (check-exn
+     #rx"serializer is closed"
+     (λ () (send serializer open))))
   (test-case "close"
-    (define-values (open close serialize) (make-serializer))
-    (check-exn #rx"serializer has not been opened" close)
-    (check-exn #rx"serializer has not been opened" (λ () (serialize #f)))
-    (open)
-    (close)
-    (check-exn #rx"serializer is closed" close)
-    (check-exn #rx"serializer is closed" (λ () (serialize #f)))))
+    (define serializer (new serializer%))
+    (check-exn
+     #rx"serializer has not been opened"
+     (λ () (send serializer close)))
+    (check-exn
+     #rx"serializer has not been opened"
+     (λ () (send serializer serialize #f)))
+    (send serializer open)
+    (send serializer close)
+    (check-exn
+     #rx"serializer is closed"
+     (λ () (send serializer close)))
+    (check-exn
+     #rx"serializer is closed"
+     (λ () (send serializer serialize #f)))))
